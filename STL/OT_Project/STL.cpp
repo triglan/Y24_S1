6e74c18a3ff8f67d45dd88995b5d9cab881355f1
triglan <glanb4277@gmail.com>
2024-03-11 18:56:54 +0900

    STL 최대값찾기
    cout << *max_element(istream_iterator<int>{in}, {});

--- STL/OT_Project/STL.cpp
+++ STL/OT_Project/STL.cpp
@@ -1,40 +1,27 @@
 //----------------------------------------------------------
-// 2024 STL 3.5 월 9-10시								(1-1)
-//
-// Visual Studio 17.9.0 이상, Release/x64
-// Project 속성/std:c++ latest <== 미리보기 최신, SDL 검사 - NO 끄기
-// 
-// 한학기 강의를 저장하는 save 만들기
+// 2024 STL 3.5 월 9-10시		(1-1)
 // ----------------------------------------------------------
 
-#include <random>
-#include <format>
 #include <iostream>
 #include "save.h"
+
+#include <string_view>
+#include <fstream>
+#include <algorithm>
 using namespace std;
 
-default_random_engine dre;
-uniform_int_distribution uid{ 1000, 9999 };
-
-// 문제 가장 큰 수를 찾아 화면에 출력하라
+	
 int main()
 {
-	int a[1000];
+	//문제 : 파일 int값들.txt에는 몇개인지 모르는 int가 저장되어 있다.
+	//모두 몇개인지 출력하고 그 중에 가장 큰 값도 화면에 출력하시오
+
+	ifstream in{ "int값들.txt" };
+	if (not in)
+		exit(0);
 	
-	for (int& num : a)
-		num = uid(dre);
+	cout << *max_element(istream_iterator<int>{in}, {});
+	
 
-	for (int num : a)
-		cout << format("{:8}", num);
-
-	// 가장 큰 수를 찾는 프로그램
-	int big{ numeric_limits<int>::min()}; // int가 가질 수 있는 가장 최소의 값.
-	for (int num : a) {
-		if (big < num) {
-			big = num;
-		}
-	}
-
-	cout << endl << endl << "가장 큰 수 : " << big << endl;
 	save("STL.cpp");
 }
