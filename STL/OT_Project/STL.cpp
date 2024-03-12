2637e9ed7df5237ee8fce6f3cb64b0569e635244
triglan <glanb4277@gmail.com>
2024-03-12 16:48:42 +0900

    파일에 기록된 int값 100개를 메모리로 읽어온 후 화면에 그 값을 출력하라.

--- STL/OT_Project/STL.cpp
+++ STL/OT_Project/STL.cpp
@@ -4,32 +4,32 @@
 
 #include <iostream>
 #include "save.h"
-#include <array>
-#include <print>
-#include <algorithm>
-#include <numeric>
 #include <fstream>
+#include <array>
+
+
 
 using namespace std;
 
-	
+// 문제 : "int100개를메모리그대로저장.txt"에
+// int값 100개가 write 함수를 사용하여 기록되어 있다.
+// 파일은 binary 모드로 열어 기록되었다. 
+// 파일에 기록된 int값 100개를 메모리로 읽어온 후
+// 화면에 그 값을 출력하라.
+
 int main()
 {
-	//문제 : int 100개를 저장하는데 메모리 bit값을 그대로 저장하자.
-	//int 100개 = sizeof(int) * 100 = 400 bytes
+	ifstream in("int100개를메모리그대로저장.txt", ios::binary );
+	if (not in)
+		exit(0);
 
-	array<int, 100>a{};
+	array<int, 100>a;
 
-	iota(a.begin(), a.end(), 1);
+	in.read((char*)a.data(), 400);
 
-	ofstream out{ "int100개를메모리그대로저장.txt", ios::binary };
-	//로우레벨 i/o를 사용한다.
-	out.write( (const char*)(a.data()), a.size() * sizeof(int)); // 얘보다 빠른 코드는 없다. io 장치끼리 데이터를 교환함.
-
-	//401바이트가 나오네 왜? OS가 중간에 개입했기 때문이야
-	//ofstream out에서 txt파일로 연 순간 저수준 io를 사용한순간 바이트를 변환하면서
-	//바이트가 늘어난다. ios::binary로 바이트 변환 안한다고 알려주면 400바이트 그대로 나온다
-	
+	for (int num : a)
+		cout << num << "\t";
+	cout << endl;
 
 	save("STL.cpp");
 }
