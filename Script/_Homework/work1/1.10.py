f8b4418674a07583620b58cae5706c469c9bffba
triglan <glanb4277@gmail.com>
2024-03-13 20:58:25 +0900

    python lecture1

---
+++ Script/_Homework/work1/1.10.py
@@ -0,0 +1,9 @@
+km = 14
+minute = 45.5
+
+mile = km * 1.6
+hour = minute / 60
+
+milePerHour = mile/hour
+
+print('시속(마일) : ', milePerHour)
---
+++ Script/_Homework/work1/1.9.py
@@ -0,0 +1,5 @@
+width = 4.5
+height = 7.9
+area = width * height
+
+print('사각형의 넓이 : ', int(area))
