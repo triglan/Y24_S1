95b41a926d80b96ddac42bcbac95fb0a292eff1c
triglan <glanb4277@gmail.com>
2024-04-09 17:19:34 +0900

    STL 6-2

--- STL/OT_Project/STL.cpp
+++ STL/OT_Project/STL.cpp
@@ -1,32 +1,28 @@
 //----------------------------------------------------------
-// 2024 STL 4.8 화 (6-1주차)
+// 2024 STL 4.9 화 (6-2주차)
 //  
-// STL - Container
-// 표준 컨테이너란? - Containers are objects that store other objects.
+// vector - dynamic array
 // ----------------------------------------------------------
 
 #include <iostream>
-#include <array>
+#include <vector>
+#include <numeric>
+#include <algorithm>
 #include "save.h"
 #include "String.h"
 using namespace std;
-
 extern bool 관찰;
 
 int main()
 {			
-	// [문제] a에 "stl.cpp"에 있는 단어를 저장하라.
-	// 길이 오름차순으로 정렬하라
-	// 화면에 a를 출력하라
-	array<String, 100>a;
+	// [문제] 키보드에서 입력한 String을 길이오름차순 정렬하여 출력하라.
+	vector<String> v{ istream_iterator<String>{cin}, {} };//키보드 입력 시작부터 입력 끝까지 저장하라.
+	
+	sort(v.begin(), v.end(), [](const String& a, const String& b) {
+		return a.getLen() < b.getLen();
+		});
 
-	ifstream in{ "STL.cpp" };
-	if (not in)
-		return 20240408;
-
-	for (int i = 0; i < 100; ++i)
-		in >> a[i];
-		
-
+	for ( const String s : v)
+		cout << s << " ";
 	save("STL.cpp");
 }
--- STL/OT_Project/String.cpp
+++ STL/OT_Project/String.cpp
@@ -1,91 +1,114 @@
-//-------------------------------------------------------
-// String.cpp STL 동작을 관찰하기 위한 클래스
+//-----------------------------------------------------------------------------------
+// String.cpp	STL 동작을 관찰하기 위한 클래스
 // 
-// 2024/4/2 시작
-//-------------------------------------------------------
+// 2024.04.02	시작 
+//-----------------------------------------------------------------------------------
 #include "String.h"
-//관찰용 변수 추가 - 2024.4.2
-bool 관찰{ false };//관찰을 원하면 true로 바꾸자
 
-size_t String::uid{};// class (private)Static 변수 초기화
+// 관찰용 변수 추가 - 2024.04.02
+bool 관찰{ false };			// 관찰을 원하면 true로 바꾸자
 
+size_t String::uid{ };		// class static 변수 초기화
 
-//디폴트 생성자2024.4.2
+// 디폴트 생성자 - 2024.04.02
 String::String()
-    : id{ ++uid }
+	: id{ ++uid }
 {
-    if (관찰)
-        std::cout << "[" << id << "] - 생성, 길이 -" << len << ", 주소 - " << (void*)p.get() << std::endl;
-
+	if (관찰)
+		std::cout << "[" << id << "] - 생성(), 길이 - " << len << ", 주소 - " << (void*)p.get() << std::endl;
 }
-
 
 // 생성자
 String::String(const char* s)
-    : len(strlen(s)), id{ ++uid }
+	: len(strlen(s)), id{ ++uid }
 {
-    p = std::make_unique<char[]>(len);
-    memcpy(p.get(), s, len);
-    if (관찰)
-        std::cout << "[" << id << "] - 생성(char*), 길이 -" << len << ", 주소 - " << (void*)p.get() << std::endl;
+	p = std::make_unique<char[]>(len);
+	memcpy(p.get(), s, len);
+	if (관찰)
+		std::cout << "[" << id << "] - 생성(char*), 길이 - " << len << ", 주소 - " << (void*)p.get() << std::endl;
 }
 
-String::~String() {
-    if (관찰)
-        std::cout << "[" << id << "] - 소멸 길이 -" << len << ", 주소 - " << (void*)p.get() << std::endl;
+// 소멸자는 관찰하려고 코딩함 - 2024.04.02
+String::~String()
+{
+	if (관찰)
+		std::cout << "[" << id << "] - 소멸, 길이 - " << len << ", 주소 - " << (void*)p.get() << std::endl;
 }
 
 
-//복사생성과 복사할당 - 2024.4.2
+// 복사생성과 복사할당 - 2024.04.02
 String::String(const String& other)
-    : len(other.len), id{ ++uid }
+	: len(other.len), id{ ++uid }	
 {
-    p = std::make_unique<char[]>(len);
-    memcpy(p.get(), other.p.get(), len);
-    if (관찰)
-        std::cout << "[" << id << "] - 복사생성(char*), 길이 -" << len << ", 주소 - " << (void*)p.get() << std::endl;
-
+	p = std::make_unique<char[]>(len);
+	memcpy(p.get(), other.p.get(), len);
+	if (관찰)
+		std::cout << "[" << id << "] - 복사생성, 길이 - " << len << ", 주소 - " << (void*)p.get() << std::endl;
 }
 
-
-
-String& String::operator = (const String& rhs)
+String& String::operator=(const String& rhs)
 {
-    if (this == &rhs)
-        return *this;
+	if (this == &rhs)
+		return *this;
 
-    p.release();
+	p.release();
 
-    len = rhs.len;
-    p = std::make_unique<char[]>(len);
+	len = rhs.len;
+	p = std::make_unique<char[]>(len);
 
-    memcpy(p.get(), rhs.p.get(), len);
-    if (관찰)
-        std::cout << "[" << id << "] - 복사할당, 길이 -" << len << ", 주소 - " << (void*)p.get() << std::endl;
+	memcpy(p.get(), rhs.p.get(), len);
+	if (관찰)
+		std::cout << "[" << id << "] - 복사할당, 길이 - " << len << ", 주소 - " << (void*)p.get() << std::endl;
 
-    return *this;
+	return *this;
 }
 
-//이동생성과 이동할당 -2024.4.2
+// 이동생성과 이동할당 - 2024.04.02
 String::String(String&& other)
-    :len(other.len), id{ ++uid } {
-    p.reset(other.p.release());
-    if (관찰)
-        std::cout << "[" << id << "] - 이동생성, 길이 -" << len << ", 주소 - " << (void*)p.get() << std::endl;
-}
-String& String::operator=(String&& other) {
-    if (this == &other) return *this;
-    p.reset();
-    len = other.len;
-    p.reset(other.p.release());
-    if (관찰)
-        std::cout << "[" << id << "] - 이동할당, 길이 -" << len << ", 주소 - " << (void*)p.get() << std::endl;
-    return *this;
-}
-size_t String::getLen() const {
-    return len;
+	: len(other.len), id(++uid)
+{
+	p.reset(other.p.release());	// 공부해라
+	if (관찰)
+		std::cout << "[" << id << "] - 이동생성, 길이 - " << len << ", 주소 - " << (void*)p.get() << std::endl;
 }
 
-char* String::getMem() const {
-    return p.get();
+String& String::operator=(String&& other)
+{
+	if (this == &other)
+		return *this;
+
+	p.reset();
+
+	len = other.len;
+
+	p.reset(other.p.release());	// 공부해라
+	if (관찰)
+		std::cout << "[" << id << "] - 이동할당, 길이 - " << len << ", 주소 - " << (void*)p.get() << std::endl;
+
+	return *this;
+}
+
+
+// sort에서 사용하기 위한 getLen - 2024.04.02
+size_t String::getLen() const
+{
+	return len;
+}
+
+// p가 가리키는 메모리를 알려준다 - 2024.04.02
+char* String::getMem() const
+{
+	return p.get();
+}
+
+// 2024.04.09
+std::istream& operator>>(std::istream& is, String& s)
+{
+	std::string ts;
+	is >> ts;
+	s.len = ts.size();
+	s.p = std::make_unique<char[]>(s.len);
+	memcpy(s.p.get(), ts.data(), s.len);
+
+	return is;
 }
