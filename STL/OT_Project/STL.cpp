6a038c1d92cf58687e707574e252b5d358aa763a
triglan <121035322+triglan@users.noreply.github.com>
2024-04-22 11:49:16 +0900

    stl

--- STL/OT_Project/STL.cpp
+++ STL/OT_Project/STL.cpp
@@ -1,37 +1,52 @@
-//----------------------------------------------------------
-// 2024 STL 4.16 화 (7-2주차)
-//  
-// 4/23 중간시험
-// deque - vector와 list의 중간형태를 띄는 자료구조다.
-// ----------------------------------------------------------
-
 #include <iostream>
 #include "save.h"
-#include "String.h"
-
-#include <vector>
-#include <deque>
-#include <list>
+#include <random>
+#include <array>
+#include <fstream>
 using namespace std;
-extern bool 관찰;
 
-class Test {
-	char x[4'096];
+default_random_engine dre;
+uniform_int_distribution<int> uidChar{ 'a','z' };
+uniform_int_distribution uidNum{ 1, 99999 };
+
+class Dog {
+	char c{ (char)uidChar(dre) };
+	int num{ uidNum(dre) };
+
+public:
+	Dog() {
+		cout << "디폴트 생성" << endl;
+	}
+	friend ostream& operator<<(ostream& os, const Dog& dog) {
+		return os << "글자 : " << dog.c << ", 숫자 : " << dog.num;
+	}
 };
+
 int main()
 {
-	// vector<Test> v;//236'2204
-	list<Test> v;
-	while (true) {
-		try{
-			v.push_back(Test{});
-		}
-		catch (...) {
-			cout << "끝 : " << v.size() << endl;
-			break;
-		}
-		if (!(v.size() % 10'00000))
-			cout << v.size() << " - Still running..." << endl;
-	}
-	save("STL.cpp");
+	array<Dog, 100> dogs{};
+	for (const Dog& dog : dogs)
+		cout << dog << endl;
+
+	//문제 : dogs를 binary mode/write로 파일에 기록해라.
+	//기록된 파일을 읽어서 num값이 가장 큰 dog를 찾아 화면에 출력
+
+	cout << sizeof dogs << endl;
+	cout << typeid(dogs).name() << endl;
+
+	ofstream out{ "dogsData.txt", ios::binary };
+	out.write((const char*)dogs.data(), dogs.size() * sizeof(int) * 2);
+
+	ifstream in("dogsData.txt", ios::binary);
+	if (not in)
+		exit(0);
+
+	array<Dog, 100> indogs{};
+
+	in.read((char*)indogs.data(), dogs.size() * sizeof(int) * 2);
+
+	cout << *max_element(istream_iterator<int>{in}, {});
+
+
+	//save("STL.cpp");
 }
