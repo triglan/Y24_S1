bd8acb4fdd2ebdfe17564da5c64f60fc5272f2d5
triglan <glanb4277@gmail.com>
2024-04-26 14:54:53 +0900

    d

--- STL/OT_Project/STL.cpp
+++ STL/OT_Project/STL.cpp
@@ -1,35 +1,27 @@
-#include <iostream>
-#include "save.h"
-#include "String.h"
-#include <list>
-#include <fstream>
-#include <vector>
-#include <deque>
-using namespace std;
-extern bool 관찰;
-
-class Test {
-	char x[4'096];
-};
-int main()
-{
-	//list : 500, deque : 500, vector : 2000
-	deque<String> cont;
-	cout << "리스트의 크기 - " << sizeof cont << endl;//리스트의 크기 - 16
-	관찰 = true;
-	//[문제] "string.cpp"에 있는 단어를 cont에 저장하라
-
-	ifstream in{ "String.cpp" };
-	if (not in) return 0;
-	String s;
-	while (in >> s) {
-		cont.emplace_back(s);//이건 어떨까 복사생성이 안나올까? -> 절대 안되지 그저 복사생성될 뿐이야.
-		//s 객체가 그냥 객체니까.
-	}
-
-	관찰 = false;
-	cout << endl;
-	for (const String& s : cont)
-		cout << s << endl;
-	save("STL.cpp");
-}
+//#include <iostream>
+//#include "save.h"
+//#include "String.h"
+//#include <fstream>
+//
+//using namespace std;
+////파일읽어서 객체 몇개인지 써라
+////제일 큰 객체 찾아라
+////왜 그 컨테이너를 사용했느지
+////Sort, find, count등 다양한 알고리즘을 사요ㅕㅇ한느 문제들
+//int main()
+//{
+//    ifstream in{ "int값들.txt" };
+//    if (not in)
+//        exit(0);
+//
+//    cout << *max_element(istream_iterator<int>{in}, {}) << endl;
+//    in.clear();
+//    in.seekg(0, ios::beg);
+//
+//
+//    int num, cnt{};
+//    while (in >> num)
+//        ++cnt;
+//    cout << "모두 " << cnt << "개의 int를 읽었다." << endl;
+//    
+//}   
--- Script/_Homework/work6/BJ_10250_ACMHotel.py
+++ Script/_Homework/work6/BJ_10250_ACMHotel.py
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 #W방 H층
 T = eval(input())
 for _ in range(T):
@@ -9,3 +10,24 @@ for _ in range(T):
            if cnt == C:
                print(100 * (y+1) + (x+1))
                y,x = H,W
+=======
+# d = []
+# count = 0
+# flag = True
+# for _ in range(8):
+#     d = input()
+#     for i in range(8):
+#         if flag:
+#             if i%2 == 0 and d[i] == 'F':
+#                 count += 1
+#         else:
+#             if i%2 == 1 and d[i] == 'F':
+#                 count += 1
+#     flag = not flag
+#
+# print(count)
+
+
+cnt = 0
+for _ in range(cnt):
+>>>>>>> Stashed changes
