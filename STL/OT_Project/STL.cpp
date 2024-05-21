7463edb321b663aeb6ae001c6c81eed7cc04b3a5
triglan <glanb4277@gmail.com>
2024-05-21 17:22:50 +0900

    STL

--- STL/OT_Project/STL.cpp
+++ STL/OT_Project/STL.cpp
@@ -1,56 +1,38 @@
 //---------------------------------------------------------
-// 2024 1학기 STL 5월 20일 월요일
+// 2024 1학기 STL 5월 21일 화요일
 //
-// Associative Container - Set
+// Associative Container - map<key, value> -> dictionary
 // 
-// set은 어떻게 절렬하는가
-// 디폴트 less<Key> -> operator< 를 이용한다.
+// 독특한 기능 : map -> 연관 배열처럼 사용할 수 있다.
+// 
+// 
+// 6월 11일 화요일(15주 2) - 기말시험
 //---------------------------------------------------------
 #include <iostream>
 #include "save.h"
 #include "string.h"
-#include <set>
+#include <map>
 #include <fstream>
-#include <cctype>
-#include <algorithm>
+#include <list>
 using namespace std;
 extern bool 관찰;
 
 
 int main()
 {
-	//[문제] "이상한 나라의 앨리스.txt" 파일에 있는 단어를 multiset에 읽어 왔다.
-	
-	multiset<String> s;
+	//그룹과 멤버
+	map<String, list<String>>m{ {"아이브", {"안유진", "가을", "레이", "장원영", "리즈", "이서"}}};
+	m.insert(pair<String, list<String>> {"블랙핑크", {"지수", "제니", "로제", "리사"}});
+	m.insert(make_pair("아이유", list<String>{ "아이유" }));
+	m["르세라핌"] = { "사쿠라","김채원","허윤진","허윤진","카즈하","홍은채"};
 
-	ifstream in{ "이상한 나라의 앨리스.txt" };
-	if (!in) return 0;
-	
-	String str;
-	while (in >> str)
-		s.insert(str);
-	
-	cout << "읽은 multiset의 단어 - " << s.size() << endl;
-
-	//[문제] 찾는 단어가 set에 있는지 알려준다.
-	while (true) {
-		cout << "찾을 단어는? ";
-		String word;
-		cin >> word;
-
-		auto p = s.find(word);//이렇게 써야 한다. 아까 begin 이런식으로 하면 0점
-		//인간의 눈으론 구분하지 못하겠지만 내부에선 엄청나게 빨라졌다.
-		
-		//+ 이 단어 있어 없어를 출력해주는거 
-		bool b = s.contains(word);
-
-		if (b)//가장 빠른 이거 있어? 의 알고리즘
-			cout << word << "는 앨리스에 있는 단어 입니다." << endl;
-		else
-			cout << "그런거 없습니다." << endl;
-		
+	//전체 출력
+	for (const auto& [그룹, 멤버들] : m) {
+		cout << 그룹 << " - " << 멤버들.size() << "명, ";
+		for (const auto& 멤버 : 멤버들)
+			cout << 멤버 << " ";
+		cout << endl;
 	}
-
 	save("STL.cpp");
 }
 
