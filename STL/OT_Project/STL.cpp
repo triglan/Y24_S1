3676113995f4bd54a113764411a9daf23dae496d
triglan <121035322+triglan@users.noreply.github.com>
2024-06-11 14:04:27 +0900

    STL

--- STL/OT_Project/STL.cpp
+++ STL/OT_Project/STL.cpp
@@ -1,34 +1,23 @@
-//---------------------------------------------------------
-// 2024 1학기 STL 6월 3일 화요일
-//
-// 6/11 화요일 - 기말시험
-// 
-// C++20 Concept / Range
-//---------------------------------------------------------
 #include <iostream>
 #include "save.h"
-#include "string.h"
-#include <algorithm>
+#include "String.h"
+#include <array>
+#include <vector>
 #include <list>
+#include <print>
+#include <random>
+#include <algorithm>
+#include <numeric>
 #include <ranges>
-
 using namespace std;
 extern bool 관찰;
-
-template<class T>
-//정수 or 부동소수점수
-concept 숫자만 = is_integral_v<T> or is_floating_point_v<T>;
-
-template<숫자만 T>
-T add(T a, T b) {
-    return a + b;
-}
-
-int main()
-{
-    list<int> list{ 1,2,3 };
-
-    ranges::sort(list.begin(), list.end(),);
-
+default_random_engine dre;
+int main() {
     save("STL.cpp");
+    vector<int> v(100);
+    ranges::iota(v, 1); //constrained-algorithm (컨셉으로 인자에 제약을 가한 함수)
+    ranges::shuffle(v, dre);
+    ranges::sort(v.begin(), v.begin() + 20);
+    for (int num : views::filter(v, [](int n) { return n & 1; })) //[begin, predicate)
+        print("{:4}", num);
 }
--- STL/OT_Project/save.cpp
+++ STL/OT_Project/save.cpp
@@ -12,31 +12,31 @@
 void save(std::string_view fileName)
 {
     // fileName을 읽을 파일로 연다.
-    std::ifstream in{ fileName.data() };
-    if (not in) {
-        std::cout << fileName << " - 파일 열기 실패" << std::endl;
-        exit(0);
-    }
+    //std::ifstream in{ fileName.data() };
+    //if (not in) {
+    //    std::cout << fileName << " - 파일 열기 실패" << std::endl;
+    //    exit(0);
+    //}
 
-    // 저장할 파일을 덧붙여 쓰기 모드로 연다.
-    std::ofstream out{ "0604_14_2_강의저장.txt", std::ios::app };
+    //// 저장할 파일을 덧붙여 쓰기 모드로 연다.
+    //std::ofstream out{ "0604_14_2_강의저장.txt", std::ios::app };
 
-    // 저장한 시간을 기록한다.
-    out << std::endl << std::endl << std::endl;
-    auto now = std::chrono::system_clock::now();               // epoch(찾아보셈)
-    auto time = std::chrono::system_clock::to_time_t(now);         // UTC 시간으로 변환
-    auto lt = localtime(&time);   // 현지시간으로 변환(컴퓨터 시간 따라감)
+    //// 저장한 시간을 기록한다.
+    //out << std::endl << std::endl << std::endl;
+    //auto now = std::chrono::system_clock::now();               // epoch(찾아보셈)
+    //auto time = std::chrono::system_clock::to_time_t(now);         // UTC 시간으로 변환
+    //auto lt = localtime(&time);   // 현지시간으로 변환(컴퓨터 시간 따라감)
 
-    auto old = out.imbue(std::locale("ko_KR"));
-    // 날짜를 내 마음대로 출력한다.
-    out << "================================================" << std::endl;
-    out << fileName << std::put_time(lt, ", 저장시간: %x %A %X") << std::endl;
-    out << "================================================" << std::endl;
-    out.imbue(old);
+    //auto old = out.imbue(std::locale("ko_KR"));
+    //// 날짜를 내 마음대로 출력한다.
+    //out << "================================================" << std::endl;
+    //out << fileName << std::put_time(lt, ", 저장시간: %x %A %X") << std::endl;
+    //out << "================================================" << std::endl;
+    //out.imbue(old);
 
 
-    // 읽을 파일의 내용을 모두 읽어 쓸 파일에 덧붙여 쓴다.
-    // STL 자료구조와 알고리즘을 이용해 본다. (좋은 코딩은 아님, 예를 들어서 보여줄 뿐...)
-    std::vector<char> v{ std::istreambuf_iterator<char>{in}, {} };
-    copy(v.begin(), v.end(), std::ostreambuf_iterator<char>{out});
+    //// 읽을 파일의 내용을 모두 읽어 쓸 파일에 덧붙여 쓴다.
+    //// STL 자료구조와 알고리즘을 이용해 본다. (좋은 코딩은 아님, 예를 들어서 보여줄 뿐...)
+    //std::vector<char> v{ std::istreambuf_iterator<char>{in}, {} };
+    //copy(v.begin(), v.end(), std::ostreambuf_iterator<char>{out});
 }
